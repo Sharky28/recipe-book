6d286afd2c18627086a62ca9a7ad1a137609aac6
Sharmarke <sharmarke.ugas@gmail.com>
2017-01-20 14:40:12 +0000

    added directives to handle dropdown

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,14 @@ import {Recipe} from "../recipe";
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] =[];
+  recipes: Recipe[] =[
+
+    new Recipe('Schnitzel','Very tasty','http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg',[]),
+    new Recipe('Summer salad','Okayish','http://cdn.iowagirleats.com/wp-content/uploads/2013/05/Triple-Berry-Summer-Salad-03_mini.jpg',[])
+  ];
   @Output() recipeSelected =   new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy','Dummy','http://thumbs2.ebaystatic.com/d/l225/m/mfXELL6zPWJE4OC0agiXMZw.jpg')
+
+
 
   constructor() { }
 
